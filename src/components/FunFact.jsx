faea338f26b72adc75e0786cf3d7eef838f34824
Sean C Davis <scdavis41@gmail.com>
2021-08-13 06:18:26 -0400

    Facts expand and collapse

--- src/components/FunFact.jsx
+++ src/components/FunFact.jsx
@@ -1,9 +1,20 @@
+import { useState } from "react";
 import styles from "./FunFact.module.css";
 
 export default function FunFact({ children, heading, sourceUrl }) {
+  const [expanded, setExpanded] = useState(false);
+
+  let wrapperClasses = styles.wrapper;
+  if (expanded) wrapperClasses += ` ${styles.wrapperExpanded}`;
+
   return (
-    <div className={styles.wrapper}>
-      <h2 className={styles.heading}>{heading}</h2>
+    <div className={wrapperClasses}>
+      <h2 className={styles.heading}>
+        <button onClick={() => setExpanded(!expanded)}>
+          <span>{expanded ? "🟣" : "⚪️"}</span>
+          <span>{heading}</span>
+        </button>
+      </h2>
       <p className={styles.fact}>{children}</p>
       <p>
         <a href={sourceUrl}>Source</a>
